697a129a3b70853e41835fa5b61fbc560f2fbf42
Alesha Tikhonov <zukko961@gmail.com>
2021-03-15 20:56:27 +0300

    remove mistakes

--- vendor/JavaScript.js
+++ vendor/JavaScript.js
@@ -1,14 +1,17 @@
 let popup = document.querySelector('.popup');
 let createButton = document.querySelector('.profile-block__link');
 let buttonClose = document.querySelector('.popup__close');
-let popupOverlay = document.querySelector('.popup__overlay');
+// let popupOverlay = document.querySelector('.popup__overlay');
 let popupButton = document.querySelector('.popup__button');
+let popupContainer = document.querySelector('.popup__container');
+/////////////////////////////////////////////////////
 function openButton() {
     popup.classList.add('popup_opened');
 }
 function closeButton() {
     popup.classList.remove('popup_opened');
 }
+//////////////////////////////////////////////////////
 createButton.addEventListener('click', function(e) {
     openButton();
 
@@ -16,13 +19,18 @@ createButton.addEventListener('click', function(e) {
 buttonClose.addEventListener('click', function(e) {
     closeButton();
 })
-
-popupOverlay.addEventListener('click', function(e) {
+//-------------------- //
+popup.addEventListener('click', function(e) {
     closeButton();
 })
+popupContainer.addEventListener('click', function(e) {
+    e.stopImmediatePropagation();
+})
+//--------------------//
 popupButton.addEventListener('click', function(e) {
     closeButton();
 })
+///////////////////////////////////////////////////////
 let nameTitle = document.getElementById('title');
 let nameJob =  document.getElementById('subtitle');
 let nameInput = document.querySelector('.popup__name'); 
